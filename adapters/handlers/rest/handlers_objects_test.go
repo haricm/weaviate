5b36f5bd9c480d4c2385081e8798e87e55b68d0b
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2022-05-23 18:57:43 +0200

    fix mock

--- adapters/handlers/rest/handlers_objects_test.go
+++ adapters/handlers/rest/handlers_objects_test.go
@@ -670,7 +670,7 @@ func (f *fakeManager) ValidateObject(_ context.Context, _ *models.Principal, _ *
 	panic("not implemented") // TODO: Implement
 }
 
-func (f *fakeManager) GetObject(_ context.Context, _ *models.Principal, _ strfmt.UUID, _ additional.Properties) (*models.Object, error) {
+func (f *fakeManager) GetObject(_ context.Context, _ *models.Principal, class string, _ strfmt.UUID, _ additional.Properties) (*models.Object, error) {
 	return f.getObjectReturn, nil
 }
 
