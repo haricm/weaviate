74bf4e62c79eb6cf9a3e994ad7427558803fc34a
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2022-05-23 10:05:07 +0200

    TD-34:DB: add new method to get objects using class name and id

--- adapters/repos/db/crud.go
+++ adapters/repos/db/crud.go
@@ -125,6 +125,28 @@ func (d *DB) ObjectByID(ctx context.Context, id strfmt.UUID,
 	return d.enrichRefsForSingle(ctx, result, props, additional)
 }
 
+// Object gets object with id from index of specified class.
+func (d *DB) Object(ctx context.Context, className string,
+	id strfmt.UUID, props search.SelectProperties,
+	adds additional.Properties) (r *search.Result, err error) {
+	idx := d.GetIndex(schema.ClassName(className))
+	if idx == nil {
+		return nil, fmt.Errorf("index not found for class %s", className)
+	}
+
+	obj, err := idx.objectByID(ctx, id, props, adds)
+	if err != nil {
+		return nil, errors.Wrapf(err, "search index %s", idx.ID())
+	}
+	if obj != nil {
+		r = obj.SearchResult(adds)
+	}
+	if r == nil {
+		return nil, nil
+	}
+	return d.enrichRefsForSingle(ctx, r, props, adds)
+}
+
 func (d *DB) enrichRefsForSingle(ctx context.Context, obj *search.Result,
 	props search.SelectProperties, additional additional.Properties) (*search.Result, error) {
 	res, err := refcache.NewResolver(refcache.NewCacher(d, d.logger)).
