8fb09e220199231e2bd9c021e344c6e604b2c304
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2022-06-24 10:39:13 +0200

    usecases: first impl of DeleteObjectReference

--- usecases/objects/references_delete.go
+++ usecases/objects/references_delete.go
@@ -13,12 +13,130 @@ package objects
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/go-openapi/strfmt"
 	"github.com/semi-technologies/weaviate/entities/additional"
 	"github.com/semi-technologies/weaviate/entities/models"
+	"github.com/semi-technologies/weaviate/entities/schema"
+	"github.com/semi-technologies/weaviate/usecases/objects/validation"
 )
+
+// DeleteReferenceInput represents required inputs to delete a reference from an existing object.
+type DeleteReferenceInput struct {
+	// Class name
+	Class string
+	// ID of an object
+	ID strfmt.UUID
+	// Property name
+	Property string
+	// Reference cross reference
+	Reference models.SingleRef
+}
+
+func (m *Manager) DeleteObjectReferenceEx(
+	ctx context.Context,
+	principal *models.Principal,
+	input *DeleteReferenceInput,
+) *Error {
+	res, err := m.getObjectFromRepo(ctx, input.Class, input.ID, additional.Properties{})
+	if err != nil {
+		errnf := ErrNotFound{}
+		if errors.As(err, &errnf) {
+			if input.Class == "" { // for backward comp reasons
+				return &Error{"source object deprecated", StatusBadRequest, err}
+			}
+			return &Error{"source object", StatusNotFound, err}
+		}
+		return &Error{"source object", StatusInternalServerError, err}
+	}
+	input.Class = res.ClassName
+
+	path := fmt.Sprintf("objects/%s/%s", input.Class, input.ID)
+	if err := m.authorizer.Authorize(principal, "update", path); err != nil {
+		return &Error{path, StatusForbidden, err}
+	}
+
+	unlock, err := m.locks.LockSchema()
+	if err != nil {
+		return &Error{"cannot lock", StatusInternalServerError, err}
+	}
+	defer unlock()
+
+	validator := validation.New(schema.Schema{}, m.exists, m.config)
+	if err := input.validate(ctx, principal, validator, m.schemaManager); err != nil {
+		return &Error{"bad inputs", StatusBadRequest, err}
+	}
+	obj := res.Object()
+	if obj == nil || obj.Properties == nil {
+		return nil
+	}
+	if cause := removeReference(obj, input.Property, &input.Reference); cause != "" {
+		return &Error{cause, StatusInternalServerError, nil}
+	}
+	obj.LastUpdateTimeUnix = m.timeSource.Now()
+	err = m.vectorRepo.PutObject(ctx, obj, res.Vector)
+	if err != nil {
+		return &Error{"repo.putobject", StatusInternalServerError, err}
+	}
+	return nil
+}
+
+func (req *DeleteReferenceInput) validate(
+	ctx context.Context,
+	principal *models.Principal,
+	v *validation.Validator,
+	sm schemaManager,
+) error {
+	if err := validateReferenceName(req.Class, req.Property); err != nil {
+		return err
+	}
+	if err := v.ValidateSingleRef(ctx, &req.Reference, "validate reference"); err != nil {
+		return err
+	}
+
+	schema, err := sm.GetSchema(principal)
+	if err != nil {
+		return err
+	}
+	return validateReferenceSchema(req.Class, req.Property, schema)
+}
+
+func removeReference(obj *models.Object, propertyName string,
+	property *models.SingleRef,
+) string {
+	props := obj.Properties
+	if props == nil {
+		return ""
+	}
+
+	properties, ok := props.(map[string]interface{})
+	if !ok {
+		return "property is not well formed"
+	}
+
+	if len(properties) == 0 || properties[propertyName] == nil {
+		return ""
+	}
+
+	refs, ok := properties[propertyName].(models.MultipleRef)
+	if !ok {
+		return "reference list is not well formed"
+	}
+	if len(refs) == 0 {
+		return ""
+	}
+	newrefs := make(models.MultipleRef, 0, len(refs))
+	for _, ref := range refs {
+		if ref.Beacon != property.Beacon {
+			newrefs = append(newrefs, ref)
+		}
+	}
+	properties[propertyName] = newrefs
+	//obj.Properties = props
+	return ""
+}
 
 // DeleteObjectReference from connected DB
 func (m *Manager) DeleteObjectReference(ctx context.Context, principal *models.Principal,
